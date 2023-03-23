2ffca129d59d5a32446e6874cc3976ac929f1148
feriel214 <feriel.foufa214@gmail.com>
2023-03-23 22:56:23 +0100

    backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import Home from "./pages/home/Home";
 import TopBar from "./components/topBar/TopBar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
+  const currentUser = true;
   return (
     <div className="App">
-      <TopBar />
-       {/** <Home /> */}  
-       <Single />   
+      <Router>
+        <TopBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/posts" element={<Home />} />
+          {currentUser ? (
+            <Route exact path="/" element={<Home />} />
+          ) : (
+            <Route exact path="/register" element={<Register />} />
+          )}
+          {currentUser ? (
+            <Route exact path="/login" element={<Home />} />
+          ) : (
+            <Route exact path="/login" element={<Login />} />
+          )}
+          <Route path="/post/:id" element={<Single />} />
+
+          {currentUser ? (
+            <Route exact path="/write" element={<Write />} />
+          ) : (
+            <Route exact path="/write" element={<Login />} />
+          )}
+          {currentUser ? (
+            <Route exact path="/settings" element={<Settings />} />
+          ) : (
+            <Route exact path="/settings" element={<Login />} />
+          )}
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts.jsx";
 import Sidebar from "../../components/sidebar/Sidebar.jsx";
+import Write from "../write/Write";
 import "./home.css";
 
 function Home() {
